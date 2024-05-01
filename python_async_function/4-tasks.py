895f82a361dc6a30d87709dd2cbb5f87d4dcb15d
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-05-01 18:05:31 +0400

    Update 4-tasks.py

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -10,4 +10,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def task_wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
     """Wait for a random amount of time, n times"""
-    return [await wait_random(max_delay) for _ in range(n)]
+    tasks = []
+    for _ in range(n):
+        tasks.append(wait_random(max_delay))
+    return [await task for task in asyncio.as_completed(tasks)]
