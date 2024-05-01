9e4a094b4abbc004c4dbf7b4b1f71c0bf8815086
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-05-01 17:48:32 +0400

    Update 0-basic_async_syntax.py

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10) -> int:
+async def wait_random(max_delay: int = 10) -> float:
     """Wait for a random amount of time"""
     delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
