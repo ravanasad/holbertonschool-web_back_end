2be823acd2a267c022ed90c31873717b9a6871e1
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-05-04 09:31:46 +0400

    Update 0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """async generator"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """async generator"""
     for _ in range(10):
         await asyncio.sleep(1)
