1e87b4a9a9c1174566a1976bc0661b6b8afcf9f3
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-05-11 22:38:10 +0400

    Updated

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -6,4 +6,5 @@ import pymongo
 def schools_by_topic(mongo_collection, topic):
     """ returns the list of school having a specific topic """
     documents = mongo_collection.find({"topics": topic})
-    return documents
+    arr = [doc for doc in documents]
+    return arr
--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,18 +4,19 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
 
-    n_logs = collection.count_documents({})
+    n_logs = nginx_collection.count_documents({})
     print(f'{n_logs} logs')
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print('Methods:')
     for method in methods:
-        count = collection.count_documents({"method": method})
+        count = nginx_collection.count_documents({"method": method})
         print(f'\tmethod {method}: {count}')
 
-    status_check = collection.count_documents(
+    status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
 
